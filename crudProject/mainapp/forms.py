23fa45e283966adf7f864d3bc0793a128ded06b3
Amit Joe <joe.amit@yahoo.com>
2023-05-01 00:01:53 +0530

    Update forms.py

--- crudProject/mainapp/forms.py
+++ crudProject/mainapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from mainapp.models import student_model
 
 class student_form(forms.ModelForm):
-    #name=forms.CharField(max_length=50)
+    #name=forms.CharField()
     #roll=forms.IntegerField()
     #marks=forms.IntegerField()
     class Meta:
